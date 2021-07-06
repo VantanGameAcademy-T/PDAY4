d743e01694ec8683ede62ec15ad77b997fd59445
VgatTeacher <vgat.teacher@gmail.com>
2021-07-06 18:45:40 +0900

    連鎖の最小数を設定してそれを超えてたら消すようにした。タイトルシーンを追加した。連鎖のラインを表示するようにした。

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -8,7 +8,10 @@ public class Bird : MonoBehaviour
     public GameObject[] birdPrefabs;
 
     // 連鎖判定用の鳥の距離
-    const float birdDistance = 1.4f;
+    const float BirdDistance = 1.6f;
+
+    // 最低連鎖数
+    const int MinChain = 3;
 
     // クリックされた鳥を格納
     private GameObject firstBird;
@@ -16,7 +19,11 @@ public class Bird : MonoBehaviour
     private string currentName;
     List<GameObject> removableBirdList = new List<GameObject>();
 
-     void Start()
+    // 連鎖が分かるようにラインを引く
+    public GameObject lineObj;
+    List<GameObject> lineBirdList = new List<GameObject>();
+
+    void Start()
     {
         TouchManager.Began += (info) =>
         {
@@ -33,6 +40,7 @@ public class Bird : MonoBehaviour
                     lastBird = hitObj;
                     currentName = hitObj.name;
                     removableBirdList = new List<GameObject>();
+                    lineBirdList = new List<GameObject>();
                     PushToBirdList(hitObj);
                 }
             }
@@ -54,6 +62,13 @@ public class Bird : MonoBehaviour
                 if (hitObj.tag == "bird" && hitObj.name == currentName
                     && lastBird != hitObj && 0 > removableBirdList.IndexOf(hitObj))
                 {
+                    float distance = Vector2.Distance(hitObj.transform.position, 
+                        lastBird.transform.position);
+                    if (distance > BirdDistance)
+                    {
+                        return;
+                    }
+                    PushToLineList(hitObj, lastBird);
                     lastBird = hitObj;
                     PushToBirdList(hitObj);
                 }
@@ -61,17 +76,52 @@ public class Bird : MonoBehaviour
         };
         TouchManager.Ended += (info) =>
         {
-            foreach(GameObject obj in removableBirdList)
+            int count = removableBirdList.Count;
+            if (count >= MinChain)
+            {
+                // リストに格納されている鳥を消去
+                foreach (GameObject obj in removableBirdList)
+                {
+                    Destroy(obj);
+                }
+                StartCoroutine(DropBird(count));
+            }
+
+            foreach (GameObject obj in removableBirdList)
             {
                 ChangeColor(obj, 1.0f);
             }
+            foreach (GameObject obj in lineBirdList)
+            {
+                Destroy(obj);
+            }
             removableBirdList = new List<GameObject>();
+            lineBirdList = new List<GameObject>();
             firstBird = null;
             lastBird = null;
         };
 
         StartCoroutine(DropBird(52));
     }
+
+    private void PushToLineList(GameObject lastObj, GameObject hitObj)
+    {
+        GameObject line = Instantiate(lineObj);
+        LineRenderer renderer = line.GetComponent<LineRenderer>();
+        // 線の太さ
+        renderer.startWidth = 0.1f;
+        renderer.endWidth = 0.1f;
+        // 頂点の数
+        renderer.positionCount = 2;
+        // 頂点を設定
+        renderer.SetPosition(0, new Vector3(lastObj.transform.position.x, 
+            lastObj.transform.position.y, -1.0f));
+        renderer.SetPosition(1, new Vector3(hitObj.transform.position.x, 
+            hitObj.transform.position.y, -1.0f));
+        // リストに追加
+        lineBirdList.Add(line);
+    }
+
     private void PushToBirdList(GameObject obj)
     {
         removableBirdList.Add(obj);
